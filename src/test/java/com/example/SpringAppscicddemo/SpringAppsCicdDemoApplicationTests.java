1af20b2d458ac5986ed10bae65f5a8bd0771a36f
SrijaVasu <155647893+SrijaVasu@users.noreply.github.com>
2024-02-14 18:59:55 +0530

    first commit

---
+++ src/test/java/com/example/SpringAppscicddemo/SpringAppsCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppscicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppsCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
